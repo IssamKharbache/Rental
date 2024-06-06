3b8c935bc82814131b9022525cc3eb410a47bc1e
lefline <issamkharbache2@gmail.com>
2024-06-06 12:15:29 +0100

    configure dtf and cors

--- backend/rental_backend/rental_backend/settings.py
+++ backend/rental_backend/rental_backend/settings.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
 
+from datetime import timedelta
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -16,6 +18,46 @@ DEBUG = bool(os.environ.get('DEBUG',default=0))
 
 ALLOWED_HOSTS =  os.environ.get('DJANGO_ALLOWED_HOSTS').split(" ")
 
+AUTH_USER_MODEL = "useraccount.User"
+
+SITE_ID = 1
+
+WEBSITE_URL = 'http://localhost:8000'
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME':timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME':timedelta(days=7),
+    'ROTATE_REFRESH_TOKEN_':False,
+    'BLACKLIST_AFTER_ROTATION':False,
+    'UPDATE_LAST_LOGIN':True,
+    'SIGNING_KEY':"acomplexkey",
+    'ALGORIGTHM':"H5512",
+}
+#Configuring auth
+ACCOUNT_USER_MODEL_USERNAME_FIELDS = None
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_EMAIL_VERIFICATION = None
+#rest framework
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':(
+        'rest_framework_simplejtw.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES':(
+        'rest_framework_permissions.IsAuthenticated'
+    )
+}
+#cors
+CORS_ALLOWED_ORIGINS =[
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1:3000',
+]
+
+REST_AUTH = {
+    'USE_JWT':True,
+    "JWT_AUTH_HTTPONLY":False
+}
 
 # Application definition
 
@@ -26,11 +68,26 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #rest framework
+    'rest_framework',
+    'rest_framework.authtoken',
+    'rest_framework_simplejwt',
+    #authentication
+    'allauth',
+    'allauth.account',
+    'dj_rest_auth',
+    'dj_rest_auth.registration',
+    #cors 
+    'corsheaders',
+    #my apps
+    'property',
+    'useraccount',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
